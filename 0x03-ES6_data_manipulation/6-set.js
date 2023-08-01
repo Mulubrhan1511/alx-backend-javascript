ae7491004fa3e9ff785ef5f867b023e1d1f0f0a6
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-08-01 09:02:15 +0000

    mule

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,7 +1,6 @@
 /**
  * Creates a set from an array.
- * @param {Array} array - The source array.
- * @author Yoseph Tapano <https://github.com/Jo2831>
+ * @param {Array} array - The source array
  * @returns {Set}
  */
 export default function setFromArray(array) {
