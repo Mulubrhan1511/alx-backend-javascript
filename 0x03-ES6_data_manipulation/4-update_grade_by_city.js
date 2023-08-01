eec036bd9d919cf9aab1060fcd5f0eaf5d89e130
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-08-01 08:53:40 +0000

    mule

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,25 +5,27 @@
  *   firstName: String,
  *   location: String
  * }[]} students - The list of students.
- * @param {String} city - The city of students.
+ * @param {*} city - The city of students.
  * @param {{
  *   studentId: Number,
  *   grade: Number,
  * }[]} newGrades - The new grades to be given to a student.
- * @returns {{id: Number, firstName: String, location: String, grade: Number | String}[]}
+ * @returns {{id: Number, firstName: String, location: String}[]}
  */
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const defaultGrade = { grade: 'N/A' };
 
-function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const defaultGrade = 'N/A';
-      const studentGrade = newGrades.find((grade) => grade.studentId === student.id)?.grade || defaultGrade;
-      
-      return {
-        ...student,
-        grade: studentGrade,
-      };
-    });
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }
-module.exports = updateStudentGradeByCity;
